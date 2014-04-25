2b71a5559394f3de90a2b64d441c747245c19fd3
angelicon <k.angelidakis@hotmail.com>
2014-04-25 20:19:10 +0300

    pre_final
    this is the first working version.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a cache matrix object to  
+## cache its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mtx <- NULL
+        set <- function(y) {
+                x <<- y
+                mtx <<- NULL
+        }
+        get <- function() x  ## it's a function without arguments that gets x
+        setmtx <- function(theMean) mtx <<- theMean ## assigns the 'theMean' value to mtx  
+        getmtx <- function() mtx
+        list(set = set, get = get,
+             setmtx = setmtx,
+             getmtx = getmtx)
 }
 
 
-## Write a short comment describing this function
+## The  cacheSolve function computes the inverse of the matrix
+## returned by makeCacheMatrix. In case the inverse is calculated
+## cacheSolve will retrieve the cached matrix (if it is not changed).
+If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## x is an object created with makeCacheMatrix
+        mtx <- x$getmtx()  ## matrix 'mtx' is either NULL or is indeed cached
+        if(!is.null(mtx)) {
+                message("getting cached data")
+                return(mtx)
+        }
+        data <- x$get()
+        mtx <- solve(data, ...)
+        x$setmtx(mtx)
+        mtx
 }
